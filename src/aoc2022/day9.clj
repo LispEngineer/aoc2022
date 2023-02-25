0057eb97437f47ab54496140b198f430c2209400
Douglas P. Fields, Jr <symbolics@lisp.engineer>
2023-02-25 17:26:14 -0500

    Day 9 Part 2 correct

--- src/aoc2022/day9.clj
+++ src/aoc2022/day9.clj
@@ -47,7 +47,13 @@
    N---N
    O-T-O
    N---N
-   xNONx"
+   xNONx
+   --
+   Note, the x's are impossible with a single head move.
+   However, for a rope (see part 2), they become possible for
+   the previous knot to move diagonally twice. So, the knot
+   needs to move diagonally too.
+   "
   (let [;; Programmatically do our 9 no-tail-moves
         adjacents (for [x [-1 0 1] y [-1 0 1]] [x y])
         adj-map (into {} (map #(vector % [0 0]) adjacents))
@@ -64,8 +70,13 @@
                     [-2  1] [-1  1]
                     [-1  2] [-1  1]
                     [ 1  2] [ 1  1]
-                    [ 2  1] [ 1  1]}]
-    (merge adj-map ortho-map knight-map)))
+                    [ 2  1] [ 1  1]}
+        ;; Part 2 only - double diagonal moves of previous
+        diag-map {[-2 -2] [-1 -1]
+                  [-2  2] [-1  1]
+                  [ 2 -2] [ 1 -1]
+                  [ 2  2] [ 1  1]} ]
+    (merge adj-map ortho-map knight-map diag-map)))
 
 (def d9-test-raw
   "Day 9, part 1 test input, raw string."
@@ -158,5 +169,109 @@ R 2")
 ;; -----------------------------------------------------------------
 ;; Day 9 Part 2
 
+;; THis is like the above, but this time with a 10-knot rope.
+;; Let's represent the rope as follows:
+;; [[hx hy] [1x 1y] ... [8x 8y] [tx ty]]
+;; Where each one is the position of one "knot" of the rope,
+;; consisting of 10 knots.
+;;
+;; Unlike in Part 1, where the head could only make double
+;; orthographic or knights moves, a knot could actually make
+;; a double diagonal move as well, and the move delta map has
+;; to be updated to account for that.
 
+(def d9p2-test-raw
+  "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20")
 
+(def d9p2-test-lines (str/split-lines d9p2-test-raw))
+
+(def rope-start
+  "Starting position for the rope"
+  (vec (repeat 10 [0 0])))
+
+(defn update-rope
+  "Updates the new position of the rope assuming
+   the head moves :dir.
+   This consists of:
+   1. Update head directly
+   2. Update each pair starting at h+1 [h h+1]
+   "
+  [rope dir]
+  (let [h (first rope)
+        t (rest rope)
+        ;; Where the head is moving to
+        delta-h   (dir-to-delta dir)
+        ;; Where the head now is
+        new-h     (mapv + h delta-h)]
+    ;; Now, we update each entry in t with the previous one
+    (reduce (fn [acc cur]
+              (let [prev (last acc)
+                    newcur
+                    ;; See move-ht for this below exploded out
+                    (mapv + cur (tail-moves (mapv - prev cur)))]
+                (conj acc newcur)))
+      [new-h] t)))
+
+;; Test (insufficient test, but whatever.)
+(update-rope rope-start :R)
+;; => [[1 0] [0 0] [0 0] [0 0] [0 0] [0 0] [0 0] [0 0] [0 0] [0 0]]
+(update-rope (update-rope rope-start :R) :R)
+;; => [[2 0] [1 0] [0 0] [0 0] [0 0] [0 0] [0 0] [0 0] [0 0] [0 0]]
+(update-rope (update-rope (update-rope rope-start :R) :R) :R)
+;; => [[3 0] [2 0] [1 0] [0 0] [0 0] [0 0] [0 0] [0 0] [0 0] [0 0]]
+
+(defn process-rope-moves
+  "Does the actual processing of moves (seq of :dir), returning the set of all
+   positions that the tail has been in. Assumes they both start at [0 0],
+   but doesn't matter where they start for part 1."
+  [moves]
+  (letfn [;; Our reducer function takes the old head & tail positions,
+          ;; and the set of positions seen by the tail, and returns the
+          ;; new version of that given a move.
+          (r [[rope t-seen] dir]
+            (let [new-rope (update-rope rope dir)]
+              #_(println new-rope)
+              [new-rope (conj t-seen (last new-rope))]))]
+    (last (reduce r [rope-start #{(last rope-start)}] moves))))
+
+;; Test
+(process-rope-moves (make-full-directions d9-test-lines))
+;; => #{[0 0]}
+
+;; More detailed test
+(def final-set (process-rope-moves (make-full-directions d9p2-test-lines)))
+(count final-set) ;; expected 36
+;; Print the pic of where the tail has been
+(let [[min-x min-y] (reduce #(map min %1 %2) final-set)
+      [max-x max-y] (reduce #(map max %1 %2) final-set)]
+  (dorun
+    (for [y (range min-y (inc max-y))
+          x (range min-x (inc max-x))]
+      (do
+        (if (final-set [x y])
+          (print "#")
+          (print "."))
+        (when (= x max-x) (println))))))
+;#.....................
+;#.............###.....
+;#............#...#....
+;.#..........#.....#...
+;..#..........#.....#..
+;...#........#.......#.
+;....#......#.........#
+;.....#..............#.
+;......#............#..
+;.......#..........#...
+;........#........#....
+;.........########.....
+
+;; Final answer for Day 9 Part 2
+(count (process-rope-moves (make-full-directions day9-input)))
+;; => 2455 (correct)
